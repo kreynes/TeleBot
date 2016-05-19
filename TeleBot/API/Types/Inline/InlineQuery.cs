3859105e97e2bb273b95ea3995dbb3eae780a6ef
Artem Kreynes <lazyalon@gmail.com>
2016-05-19 15:50:01 +0300

    Add InlineQuery
    Begin implementing version 2 of the Telegram Bot API. (Inline messages)

---
+++ TeleBot/API/Types/Inline/InlineQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using TeleBot.API.Types;
+
+namespace TeleBot
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class InlineQuery
+    {
+        [JsonProperty(PropertyName = "id", Required = Required.Always)]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "from", Required = Required.Always)]
+        public User Sender { get; set; }
+
+        [JsonProperty(PropertyName = "location", Required = Required.Default)]
+        public Location Location { get; set; }
+
+        [JsonProperty(PropertyName = "query", Required = Required.Always)]
+        public string Query { get; set; }
+
+        [JsonProperty(PropertyName = "offset", Required = Required.Always)]
+        public string Offset { get; set; }
+    }
+}
+
