1e1c0c11333fb8e95bcfdf2513a7a4fd372317fd
Artem Kreynes <lazyalon@gmail.com>
2016-08-14 21:34:45 +0300

    Add InlineQueryResultLocation and InlineQueryResultVenue

---
+++ TeleBot/API/Types/Inline/InlineQueryResultContact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TeleBot
+{
+    public class InlineQueryResultContact
+    {
+        public InlineQueryResultContact()
+        {
+        }
+    }
+}
+
---
+++ TeleBot/API/Types/Inline/InlineQueryResultLocation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+using TeleBot.API.Types;
+
+namespace TeleBot
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class InlineQueryResultLocation
+    {
+        [JsonProperty(PropertyName = "type", Required = Required.Always)]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "id", Required = Required.Always)]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "latitude", Required = Required.Always)]
+        public float Latitude { get; set; }
+
+        [JsonProperty(PropertyName = "longitude", Required = Required.Always)]
+        public float Longitude { get; set; }
+
+        [JsonProperty(PropertyName = "title", Required = Required.Always)]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "reply_markup", Required = Required.Default, DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public InlineKeyboardMarkup ReplyMarkup { get; set; }
+
+        [JsonProperty(PropertyName = "input_message_content", Required = Required.Default, DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public object InputMessageContent { get; set; }
+
+        [JsonProperty(PropertyName = "thumb_width", Required = Required.Default, DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public string ThumbnailUrl { get; set; }
+
+        [JsonProperty(PropertyName = "thumb_width", Required = Required.Default, DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public string ThumbnailWidth { get; set; }
+
+        [JsonProperty(PropertyName = "thumb_height", Required = Required.Default, DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public string ThumbnailHeight { get; set; }
+    }
+}
